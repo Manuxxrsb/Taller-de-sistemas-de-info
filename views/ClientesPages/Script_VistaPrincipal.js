7937e779bd9ad5acabca8071179c9e0af7dd739a
Manuxxrsb <manuxxrsb@gmail.com>
2024-11-10 11:12:13 -0300

    Fix: No se permite ingresar caracteres especiales en el prompt de valor para la cantidad del medicamento que deseas adquirir

--- views/ClientesPages/Script_VistaPrincipal.js
+++ views/ClientesPages/Script_VistaPrincipal.js
@@ -18,16 +18,28 @@ function Adquirir(ID){
     .then(data => {
         let medicamento = data;
         let valor = prompt(`Ingrese la cantidad que llevaras (stock actual: ${medicamento.stock}) ` );
-        if(valor > medicamento.stock){
+        console.log("valor del promp");
+        console.log(valor);
+
+        if(valor > medicamento.stock || valor == 0 || valor === null || valor.trim() === "" || contieneCaracteresEspeciales(valor) != false){
             alert(`Por favor ingresa un valor Valido menor o igual que ${medicamento.stock}`);
             Adquirir(ID);
+        }else{
+            let precio = medicamento.precio;
+            medicamento.stock = valor;
+            medicamento.precio = (precio * valor);
+            boleta.Medicamentos.push(medicamento);
+            alert("Medicamento agregado correctamente");
         }
-        medicamento.stock = valor;
-        boleta.Medicamentos.push(medicamento);
     })
     .catch(error => console.log(error));  
     console.log("Boleta actualizada");
     console.log(boleta);
+}
+
+function contieneCaracteresEspeciales(valor) {
+    const regex = /[!@#$%^&*(),.?":{};_=+/'¿°|<>-]/;
+    return regex.test(valor);
 }
 
 function nombreUsuario(nombre,email){
@@ -42,45 +54,53 @@ function nombreUsuario(nombre,email){
 }
 
 function ObtenerMedicamentos(){
-// Obtener los medicamentos de la base de datos
-fetch('http://localhost:8080/medicamentos')
-.then(response => response.json())
-.then(data => {
-    const medicamentosContainer = document.getElementById('medicamentos');
-    const row = document.createElement('div');
-    row.classList.add('row');
-    medicamentosContainer.appendChild(row);
+    fetch('http://localhost:8080/medicamentos')
+    .then(response => response.json())
+    .then(data => {
+        const medicamentosContainer = document.getElementById('medicamentos');
+        const row = document.createElement('div');
+        row.classList.add('row');
+        medicamentosContainer.appendChild(row);
     data.forEach(medicamento => {
-        const card = document.createElement('div');
-        card.classList.add('card', 'col-md-3');
-        card.innerHTML = `
-            <img class="card-img-top" src="/views/ClientesPages/img/medicamento.png" alt="${medicamento.nombre}">
-            <div class="card-body">
-                <h5 class="card-title">${medicamento.nombre}</h5>
-                <h6 class="card-text">${medicamento.marca}</h6>
-                <p>${medicamento.descripcion}</p>
-                <button href="#" class="btn btn-primary" onclick="Adquirir(${medicamento.ID})">Adquirir</button>
-            </div>
-        `;
-        row.appendChild(card);
+        if(medicamento.stock >0){
+            const card = document.createElement('div');
+            card.classList.add('card', 'col-md-3');
+            card.innerHTML = `
+                <img class="card-img-top" src="/views/ClientesPages/img/medicamento.png" alt="${medicamento.nombre}">
+                <div class="card-body">
+                    <h2 class="card-title">${medicamento.nombre}</h5>
+                    <h6 class="card-text">${medicamento.marca}</h6>
+                    <h6>${medicamento.descripcion}</h6> <h5> $${medicamento.precio}</h5>
+                    <button class="btn btn-primary" onclick="Adquirir(${medicamento.ID})">Adquirir</button>
+                </div>
+            `;
+            row.appendChild(card);
+        }
     });
-})
-.catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
 }
 
 function Carrito(){
-    let infoBoleta = `Usuario: ${boleta.Usuario}\nEmail: ${boleta.email}\nMedicamentos: `;
+    let infoBoleta = `|||   Carrito de Compra   ||| \n\n Usuario: ${boleta.Usuario}\n Email: ${boleta.email}\n Medicamentos | [Cantidad] | $ Precio`;
+    let Subtotal = 0;
+
     boleta.Medicamentos.forEach(medicamento => {
-        infoBoleta += `\n- ${medicamento.nombre} (Cantidad: ${medicamento.stock})`;
+        infoBoleta += `\n- ${medicamento.nombre} \t ${medicamento.stock} \t $${(medicamento.precio)}`;
+        Subtotal += medicamento.precio;
     });
+    infoBoleta += `\n\nSubtotal: $${Subtotal}`;
     alert(infoBoleta);
 }
 
 function Imprimir(){
-    let infoBoleta = `Usuario: ${boleta.Usuario}\nEmail: ${boleta.email}\nMedicamentos: `;
+    let infoBoleta = `|||   Farmacias La Estrella   ||| \n||    BOLETA  ||\n\nUsuario: ${boleta.Usuario}\nEmail: ${boleta.email}\nMedicamentos | [ Cantidad ] | $ Valor`;
+    let Subtotal = 0;
     boleta.Medicamentos.forEach(medicamento => {
-        infoBoleta += `\n- ${medicamento.nombre} (Cantidad: ${medicamento.stock})`;
+        infoBoleta += `\n- ${medicamento.nombre} \t [${medicamento.stock}] \t $${medicamento.precio}`;
+        Subtotal += medicamento.precio;
     });
+    infoBoleta += `\n\nSubtotal: $${Subtotal}`;
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
     doc.text( `${infoBoleta}` , 25 ,25);
